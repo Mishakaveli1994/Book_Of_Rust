6b5b6b9342d68b7f59bf123a7bc81a8127c155c3
Mihail Topuzov <mihata1994@yahoo.com>
2025-02-28 17:23:31 +0200

    Update

--- 7_packages/packages/src/lib.rs
+++ 7_packages/packages/src/lib.rs
@@ -1,21 +1,7 @@
-mod back_of_house{
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String
-    }
+mod front_of_house;
 
-    impl Breakfast {
-        pub fn summer(toast:&str) -> Breakfast {
-            Breakfast {
-                toast:String::from(toast),
-                seasonal_fruit:String::from("peaches")
-            }
-        }
-    }
-}
+pub use crate::front_of_house::hosting;
 
-pub fn eat_at_restaurant(){
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
 }
--- 7_packages/packages/src/main.rs
+++
@@ -1,6 +0,0 @@
-mod lib;
-use lib::eat_at_restaurant;
-
-fn main() {
-    eat_at_restaurant()
-}
