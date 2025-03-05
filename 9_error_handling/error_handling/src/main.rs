9a921bc03061c7a64438c085fdf8e8edf4135eea
Mihail Topuzov <mihata1994@yahoo.com>
2025-03-05 16:58:39 +0200

    Added error handling

---
+++ 9_error_handling/error_handling/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::{File, read_to_string};
+use std::io::{self, Read};
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file_optimized() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_optimized_even_more() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+fn read_username_from_file_max_optimized() -> Result<String, io::Error> {
+    read_to_string("hello.txt")
+}
+
+fn main() {}
